31ee1b7d7986c3bf7445bd2f9fc81321b52f07ab
kksumm123 <kksumm123@gmail.com>
2021-06-20 22:31:43 +0900

    스테이트 구현 레이방식 적용

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,15 +6,65 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody2D rigid;
+    BoxCollider2D boxCol2D;
     [SerializeField] float speed = 5f;
     [SerializeField] float jumpForce = 900f;
+
+    StateType state;
+    [SerializeField]
+    StateType State
+    {
+        get { return state; }
+        set { state = value; }
+    }
+    enum StateType
+    {
+        Ground,
+        Jump,
+        Fall
+    }
+    void StateUpdate()
+    {
+        if (IsGound())
+            State = StateType.Ground;
+        if (rigid.velocity.y < 0)
+            State = StateType.Fall;
+    }
+
+    [SerializeField] float groundRayOffset = 0;
+    [SerializeField] float groundRayLength = 0.9f;
+    [SerializeField] LayerMask groundLayer;
+    private bool IsGound()
+    {
+        var pos = transform.position;
+        if (ChkRay(pos, Vector2.down, groundRayLength, groundLayer))
+            return true;
+        if (ChkRay(pos - new Vector3(groundRayOffset, 0, 0)
+            , Vector2.down, groundRayLength, groundLayer))
+            return true;
+        if (ChkRay(pos + new Vector3(groundRayOffset, 0, 0)
+            , Vector2.down, groundRayLength, groundLayer))
+            return true;
+        return false;
+    }
+
+    bool ChkRay(Vector3 pos, Vector2 dir, float length, LayerMask layer)
+    {
+        var hit = Physics2D.Raycast(pos, dir, length, layer);
+        if (hit.transform != null)
+            return true;
+        return false;
+    }
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        groundRayOffset = boxCol2D.size.x / 2;
     }
 
     void Update()
     {
+        StateUpdate();
         Move();
         Jump();
     }
@@ -44,9 +94,13 @@ public class Player : MonoBehaviour
     }
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (State == StateType.Ground)
         {
-            rigid.AddForce(new Vector2(0, jumpForce));
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                rigid.AddForce(new Vector2(0, jumpForce));
+                State = StateType.Jump;
+            }
         }
     }
 }
