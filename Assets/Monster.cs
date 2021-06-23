68cb219c0c06bdfe7572ab09e1c2d5c48c05e95f
kksumm123 <kksumm123@gmail.com>
2021-06-24 03:49:33 +0900

    몬스터.cs작성중 - MonsterWalk 구현할 거

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     Animator animator;
+    Transform playerTr;
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerTr = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        var distance = playerTr.position - transform.position;
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("AttackObj"))
         {
+            ishit = true;
             animator.Play("Hit");
         }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("AttackObj"))
+        {
+            ishit = false;
+        }
+    }
+    bool ishit = false;
 }
