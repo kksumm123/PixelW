c7678cc600d710d7a11cc38f904d3eeb5023e8a3
kksumm123 <kksumm123@gmail.com>
2021-06-24 03:39:53 +0900

    몬스터.cs추가 AttackObj 태그 추가

---
+++ Assets/Monster.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+    }
+}
