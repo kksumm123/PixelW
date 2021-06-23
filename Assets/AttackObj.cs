69a8e8bf1d7b7ea054df4717e6fdfae22380adf8
kksumm123 <kksumm123@gmail.com>
2021-06-24 03:29:10 +0900

    공격박스 로직 거의 완료

--- Assets/AttackObj.cs
+++ Assets/AttackObj.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class AttackObj : MonoBehaviour
 {
-    [SerializeField] Vector2 attackForce = new Vector3(100, 100, 0);
-
+    BoxCollider2D boxCol2D;
+    Vector2 boxCol2DSize;
+    [SerializeField] Vector2 attackForce = new Vector3(1000, 2000, 0);
+    private void Start()
+    {
+        boxCol2D = GetComponent<BoxCollider2D>();
+        boxCol2DSize = boxCol2D.size;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Monster"))
         {
             collision.GetComponent<Rigidbody2D>().AddForce(attackForce);
+            boxCol2D.size = Vector2.zero;
         }
+    }
+    private void OnDisable()
+    {
+        boxCol2D.size = boxCol2DSize;
     }
 }
