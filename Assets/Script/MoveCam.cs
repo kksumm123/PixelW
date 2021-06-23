4dff0414a47a1f8b3839feeac060a9ebbf56f2d4
kksumm123 <kksumm123@gmail.com>
2021-06-24 02:14:37 +0900

    메인카메라를 범위안에 가두기 4 레이 추가 로직구현

--- Assets/Script/MoveCam.cs
+++ Assets/Script/MoveCam.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,19 +9,40 @@ public class MoveCam : MonoBehaviour
     Transform tr;
 
     [SerializeField] float speed = 2f;
-    [SerializeField] bool canFollow = true;
+    [SerializeField] LayerMask camViewLayer;
+    [SerializeField] float camWidth;
     void Start()
-    { 
+    {
         playerTr = GameObject.Find("Player").GetComponent<Transform>();
+        camViewLayer = 1 << LayerMask.NameToLayer("CamView");
+        camWidth = 2 * transform.GetComponent<Camera>().orthographicSize * transform.GetComponent<Camera>().aspect;
         tr = transform;
     }
 
     void Update()
     {
-        if (canFollow == true)
+        var distance = new Vector3(playerTr.position.x - tr.position.x, 0, 0);
+        if (ChkViewLayer(distance) == false)
         {
-            var distance = new Vector3(playerTr.position.x - tr.position.x, 0, 0);
             transform.Translate(distance * speed * Time.deltaTime, Space.World);
         }
+    }
+
+    private bool ChkViewLayer(Vector3 distance)
+    {
+        if (ChkRay(tr.position + new Vector3(camWidth, 0, 0) + distance
+            , Vector2.left, camWidth, camViewLayer))
+            return true;
+        if (ChkRay(tr.position + new Vector3(camWidth, 0, 0) + distance
+            , Vector2.right, camWidth, camViewLayer))
+            return true;
+
+        return false;
+    }
+    bool ChkRay(Vector3 pos, Vector2 dir, float length, LayerMask layer)
+    {
+        Debug.Assert(layer != 0, "레이어 지정안됨");
+        var hit = Physics2D.Raycast(pos, dir, length, layer);
+        return hit.transform;
     }
 }
