86bce98cb7c6d55114df774d9a2ac121f78ea1b4
kksumm123 <kksumm123@gmail.com>
2021-06-25 04:33:49 +0900

    AttackBoxObj스크립트 변수명 변경

--- Assets/AttackBoxObj.cs
+++ Assets/AttackBoxObj.cs
@@ -6,21 +6,21 @@ public class AttackBoxObj : MonoBehaviour
 {
     BoxCollider2D boxCol2D;
     Vector2 boxCol2DSize;
-    Transform playerTr;
+    Transform parentTr;
     [SerializeField] Vector2 attackForce = new Vector3(1000, 2000, 0);
 
     void Start()
     {
         boxCol2D = GetComponent<BoxCollider2D>();
         boxCol2DSize = boxCol2D.size;
-        playerTr = transform.parent.GetComponent<Transform>();
+        parentTr = transform.parent.GetComponent<Transform>();
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Monster"))
         {
-            var attackDir = playerTr.forward.z;
+            var attackDir = parentTr.forward.z;
             collision.GetComponent<Rigidbody2D>().AddForce(
                 attackForce * new Vector2(attackDir, 1));
             boxCol2D.size = Vector2.zero;
