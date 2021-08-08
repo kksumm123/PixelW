14ee64be0c76734bac711445c033af0f8cbb0924
kksumm123 <kksumm123@gmail.com>
2021-08-09 04:03:54 +0900

    todolist 갱신

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -30,9 +30,9 @@ using UnityEngine;
 // clear 몬스터 다양하게
 //   ㄴ Skeleton(0), Goblin(0), Mushroon(O)
 // clear 몬스터 발판 아래로 안떨어지도록
-// clear 조작키 알림 UI 만들기
-//   ㄴ CenterNotifyUI 완성
+// todo 조작키 알림 UI 만들기
 // todo 다음 맵으로 넘어가기
+//   ㄴ CenterNotifyUI 완성
 
 public class Player : Actor
 {
