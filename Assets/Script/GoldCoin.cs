aaa36cead157d2d6b3aff2610c975ee1aa883990
kksumm123 <kksumm123@gmail.com>
2021-08-11 21:55:19 +0900

    GoldCoin.cs 주석처리

--- Assets/Script/GoldCoin.cs
+++ Assets/Script/GoldCoin.cs
@@ -30,28 +30,48 @@ public class GoldCoin : MonoBehaviour
         Debug.Assert(wallLayer != 0, "레이어 지정안됨");
         animator = GetComponentInChildren<Animator>();
 
+        // 동전에 골드값 랜덤하게 할당
         goldValue = Random.Range(goldMinValue, goldMaxValue + 1);
+
+        // 동전 소환지점 조금 위로 올림
         transform.position += new Vector3(0, 0.25f, 0);
+        // 동전 소환 각도 랜덤하게 지정(부채꼴 모양처럼 퍼짐)
         transform.rotation = Quaternion.Euler(
                         transform.rotation.eulerAngles
                         + new Vector3(0, 0, Random.Range(-rotationZValue, rotationZValue))
                         );
+        
+        // 이하 포물선 움직임 되는 동안 Update()가 실행되기때문에
+        // 중력 물리코드의 영향을 받아 점점 아래로 처짐
+
         // Rising
+        // 포물선 상승곡선을 그려줌
+        // 일정시간동안 상승함
         var endTime = Time.time + Random.Range(risingMinTime, risingMaxTime);
         while (Time.time < endTime)
         {
+            // 만약 상,하,좌,우 하나라도 벽이랑 부딪혓다면
             if (IsReachUpLeftDown() == true)
-                yield break;
+                yield break; // Rising 그만하고 나가도록
 
+            // 직선으로 움직이도록
+            // 랜덤하게 각도가 지정되었기때문에 부채꼴처럼 퍼짐
+            // 강해지는 중력에 의해 점차 포물선 상승곡선처럼 진행됨
             transform.Translate(risingSpeed * Time.deltaTime * Vector2.up);
             yield return null;
         }
+
         // Fly as parabola(포물선)
+        // 포물선 하강곡선을 그려줌
+        // 땅에 닿을 때까지 곡선으로 떨어짐
         while (IsGround() == false)
         {
             if (IsReachUpLeftDown() == true)
                 yield break;
-
+            
+            // Rising과 같은방향 직선으로 움직이도록
+            // RisingSpeed와 다르게 FlySpeed 값도 낮고, 
+            // 시간이 지나며 중력값 또한 강해져 점차 포물선 하강곡선처럼 진행됨
             transform.Translate(flySpeed * Time.deltaTime * Vector2.up);
             yield return null;
         }
