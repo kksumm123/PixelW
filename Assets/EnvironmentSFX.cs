285aef214c594de1ea0d88ca793aaf8f1056a998
kksumm123 <kksumm123@gmail.com>
2021-08-14 18:52:15 +0900

    1. EnvSFX 파일들 위치 이동 2. BGM, EnvSFX 구현 완료

--- Assets/EnvironmentSFX.cs
+++ Assets/EnvironmentSFX.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class EnvironmentSFX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    AudioSource audioSource;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] bool isPlay = true;
+    [SerializeField] float playMinDelay = 5;
+    [SerializeField] float playMaxDelay = 15;
+    [SerializeField] List<AudioClip> envSFXList;
+    IEnumerator Start()
     {
-        
+        Debug.Assert(envSFXList.Count != 0, "환경음 리스트 설정해줘야함");
+        audioSource = GetComponent<AudioSource>();
+        while (isPlay == true)
+        {
+            audioSource.clip = envSFXList[Random.Range(0, envSFXList.Count)];
+            audioSource.Play();
+            var playDelay = Random.Range(playMinDelay, playMaxDelay);
+            yield return new WaitForSeconds(playDelay);
+        }
     }
 }
