35e1b49a259991c403c15f25822a64556cf83ca1
kksumm123 <kksumm123@gmail.com>
2021-08-14 21:07:28 +0900

    Title Scene 구현 완료

--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    AudioSource audioSource;
+    CanvasGroup canvasGroup;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        canvasGroup = GameObject.Find("Canvas").transform.Find("Title").GetComponent<CanvasGroup>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            audioSource.Play();
+            StartCoroutine(LoadingCo());
+        }
+    }
+
+    IEnumerator LoadingCo()
+    {
+        // 비동기 로드
+        var progress = SceneManager.LoadSceneAsync("Main");
+        while (progress.isDone == false)
+        {
+            canvasGroup.alpha = 1 - progress.progress;
+            yield return null;
+        }
     }
 }
