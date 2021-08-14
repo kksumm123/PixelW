6bdf3b955b96e0b3aa565104c9ca8e53f5c99fe5
kksumm123 <kksumm123@gmail.com>
2021-08-14 21:08:38 +0900

    1. Todolist 갱신 2. StageManager.cs 테스트코드 삭제

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -34,7 +34,7 @@ using UnityEngine;
 // 목요일
 // clear 조작키 알림 UI 만들기
 // Clear SFX들 추가하기 - Coin습득, 공격, 피격, Environmenmt SFX, BGM
-// todo TitleScene 만들기
+// clear TitleScene 만들기
 // todo 다음 맵으로 넘어가기
 //   ㄴ CenterNotifyUI 완성
 
